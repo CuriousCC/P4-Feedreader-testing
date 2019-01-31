10b00835146eaa9946f7b001b9ac48dec3461f1f
CuriousCC <spplearn@gmail.com>
2019-01-31 18:47:57 +0000

    add test suite "RSS Feeds"

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,12 +31,22 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-
+        it('each feed has a URL defined and is not empty', function() {
+            allFeeds.forEach(feed => {
+                expect(feed.url).toBeDefined();
+            });
+        });
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+        it('each feed has a name defined and is not empty', function() {
+            allFeeds.forEach(feed => {
+                expect(feed.name).toBeDefined();
+                expect(feed.name.length).toBeGreaterThan(2);     
+            });
+        });
     });
 
 
